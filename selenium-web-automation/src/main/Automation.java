d423a6a5afc144f16f2c560146470472988e48a7
Yunus-Alacakaptan <yunusalacakaptan@gmail.com>
2021-02-25 21:24:08 +0300

    Imported and instantiated Selenium & Firefox Driver

---
+++ selenium-web-automation/src/main/Automation.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver; 
+import org.openqa.selenium.firefox.FirefoxDriver; 
+
+
+public class Automation {
+	public static void main(String[] args) {
+		
+	//Assign Firefox Driver as "driver" named variable
+	WebDriver driver = new FirefoxDriver();
+	
+	}
+}
