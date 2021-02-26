ab2d6dba55627e9b789f3cead3f3b959cc31b7dc
Yunus-Alacakaptan <yunusalacakaptan@gmail.com>
2021-02-26 22:10:52 +0300

    Added functionality for setting quantity to 2

--- selenium-web-automation/src/test/java/pages/Basket.java
+++ selenium-web-automation/src/test/java/pages/Basket.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Basket {
@@ -21,7 +22,7 @@ public class Basket {
 		 
 		 
 		 //Get the actual price of the item in the item listing
-		 String actualPrice = driver.findElement(By.xpath("//*[contains (@id, 'sp-price-discountPrice')]")).getText();
+		 String actualPrice = driver.findElement(By.xpath("//*[contains (@class, 'lastPrice')]")).getText();
 		 
 		 //action variable for mouse operations
 		   Actions action = new Actions(driver);
@@ -45,5 +46,13 @@ public class Basket {
 		   
 		   //Compare the listing price and basket price of the item
 		   Assert.assertEquals (actualPrice, basketPrice);
+		   
+		   //Click on amount dropdown
+		   WebElement selectAmount 			= driver.findElement(By.xpath("//*[contains (@class, 'number-selection')]"));
+		   selectAmount.click();
+		   
+		   //Set the quantity to 2
+		   WebElement selectNumber 			= driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/form/div/div[2]/div[2]/div[1]/div[2]/div[6]/div[2]/div[2]/div[4]/div/div[2]/select/option[2]"));
+		   selectNumber.click();
 	 }
 }
