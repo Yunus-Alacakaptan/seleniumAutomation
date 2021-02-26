a4708b5f9fe88fc8e2fb2ce36d15a9deb065b8dd
Yunus-Alacakaptan <yunusalacakaptan@gmail.com>
2021-02-26 15:15:58 +0300

    Added functionality for navigating and testing homepage of the site

--- selenium-web-automation/src/test/java/pages/Homepage.java
+++ selenium-web-automation/src/test/java/pages/Homepage.java
@@ -1,5 +1,29 @@
 package pages;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+ 
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+ 
+import org.junit.Assert;
+import junit.framework.TestCase;
 
 public class Homepage {
-		
+	public void testHomepage() {
+	// Create an instance of the driver
+	System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+    
+    // Navigate to a web page
+    driver.get("https://gittigidiyor.com");
+    
+    //Get homepage title and confirm if it's correct
+    String message                 = driver.getTitle();
+    String successMsg             = "GittiGidiyor - Türkiye'nin Öncü Alýþveriþ Sitesi";
+    Assert.assertEquals (message, successMsg);
+    
+	}
 }
