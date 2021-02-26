9dbee149a7a9369958de83637d19bd4d115f529c
Yunus-Alacakaptan <yunusalacakaptan@gmail.com>
2021-02-26 17:27:31 +0300

    Added driver variables for invoking from other classes

--- selenium-web-automation/src/test/java/testing/TestRunner.java
+++ selenium-web-automation/src/test/java/testing/TestRunner.java
@@ -1,14 +1,30 @@
 package testing;
 
-import pages.Homepage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+
+import pages.Homepage;
+import pages.Login;
 public class TestRunner {
 	public static void main(String[] args) { 
+		// Create an instance of the driver
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		
 		//Assign home variable for invoking the testHomepage function from Homepage class
-		Homepage home = new Homepage();
+		Homepage home = new Homepage(driver);
 		
 		//Navigate to homepage and confirm if it's correct
 		home.testHomepage();
+		
+		//Assign login variable for invoking the testHomepage function from Login class
+		Login login = new Login(driver);
+		
+		//Perform a login to the site and confirm it's correct
+		login.testLogin();
 	}
 }
