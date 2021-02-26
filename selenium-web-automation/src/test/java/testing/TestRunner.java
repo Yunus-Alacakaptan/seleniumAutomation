28382c8950ec087e6ca28c4870f32caac2d05e8a
Yunus-Alacakaptan <yunusalacakaptan@gmail.com>
2021-02-26 23:06:36 +0300

    Added test functions

--- selenium-web-automation/src/test/java/testing/TestRunner.java
+++ selenium-web-automation/src/test/java/testing/TestRunner.java
@@ -10,6 +10,7 @@ import org.junit.BeforeClass;
 import pages.Homepage;
 import pages.Login;
 import pages.Search; 
+import pages.Basket;
 public class TestRunner {
 	public static void main(String[] args) { 
 		// Create an instance of the driver
@@ -18,6 +19,7 @@ public class TestRunner {
 		
 		//Maximize the window upon opening, otherwise causes errors with element finding
 		driver.manage().window().maximize(); 
+		
 		
 		//Assign home variable for invoking the testHomepage function from Homepage class
 		Homepage home = new Homepage(driver);
@@ -33,8 +35,15 @@ public class TestRunner {
 		
 		//Assign search variable for invoking the testSearch function from Search class
 		Search search = new Search(driver);
-				
+						
 		//Perform a search and confirm it's correct
 		search.testSearch();
+				
+		//Assign search variable for invoking the testSearch function from Search class
+		Basket basket = new Basket(driver);
+						
+		//Perform a search and confirm it's correct
+		basket.testBasket();
+		
 	}
 }
