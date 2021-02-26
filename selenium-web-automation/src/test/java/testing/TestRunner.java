bc24b9ae7d0619563c8f406a5ae926c6892ea2f2
Yunus-Alacakaptan <yunusalacakaptan@gmail.com>
2021-02-26 15:19:39 +0300

    Added code for invoking testHomepage function from Homepage class

--- selenium-web-automation/src/test/java/testing/TestRunner.java
+++ selenium-web-automation/src/test/java/testing/TestRunner.java
@@ -1,5 +1,14 @@
 package testing;
 
-public class TestRunner {
+import pages.Homepage;
 
+public class TestRunner {
+	public static void main(String[] args) { 
+		
+		//Assign home variable for invoking the testHomepage function from Homepage class
+		Homepage home = new Homepage();
+		
+		//Navigate to homepage and confirm if it's correct
+		home.testHomepage();
+	}
 }
