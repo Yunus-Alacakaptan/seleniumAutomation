ed8cfa5512ea65ab2ddc498ad7c2d9178cde68b7
Yunus-Alacakaptan <yunusalacakaptan@gmail.com>
2021-02-26 19:18:17 +0300

    Added maximizing window functionality, along with searchbox function

--- selenium-web-automation/src/test/java/testing/TestRunner.java
+++ selenium-web-automation/src/test/java/testing/TestRunner.java
@@ -9,11 +9,15 @@ import org.junit.BeforeClass;
 
 import pages.Homepage;
 import pages.Login;
+import pages.Search; 
 public class TestRunner {
 	public static void main(String[] args) { 
 		// Create an instance of the driver
 		System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		
+		//Maximize the window upon opening, otherwise causes errors with element finding
+		driver.manage().window().maximize(); 
 		
 		//Assign home variable for invoking the testHomepage function from Homepage class
 		Homepage home = new Homepage(driver);
@@ -21,10 +25,16 @@ public class TestRunner {
 		//Navigate to homepage and confirm if it's correct
 		home.testHomepage();
 		
-		//Assign login variable for invoking the testHomepage function from Login class
+		//Assign login variable for invoking the testLogin function from Login class
 		Login login = new Login(driver);
 		
 		//Perform a login to the site and confirm it's correct
 		login.testLogin();
+		
+		//Assign search variable for invoking the testSearch function from Search class
+		Search search = new Search(driver);
+				
+		//Perform a search and confirm it's correct
+		search.testSearch();
 	}
 }
